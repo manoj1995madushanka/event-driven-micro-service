307853bffaeb1a79872b2803972fcb6f272a4eb7
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-09-17 23:04:38 +0530

    add main class and test class for twitter to kafka service

--- twitter-to-kafka-service/src/main/java/com/twitter_to_kafka_service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/twitter_to_kafka_service/TwitterToKafkaServiceApplication.java
@@ -1,4 +1,38 @@
 package com.twitter_to_kafka_service;
 
-public class TwitterToKafkaServiceApplication {
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.PostConstruct;
+
+/*
+application initialization logics
+* @PostConstruct vs ApplicationListener vs CommandLineRunner vs @EventListener
+* */
+@SpringBootApplication
+// below one is not recommended
+//@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    /*
+    * executes after dependency injection to perform initialization
+    * */
+    /*@PostConstruct
+    public void init(){
+
+    }*/
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("initiating");
+    }
 }
