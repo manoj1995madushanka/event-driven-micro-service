bbde70d3400b5a63e0f6904273178f7e592238b8
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-10-04 20:21:19 +0530

    fix run issue

--- twitter-to-kafka-service/src/main/java/com/twitter_to_kafka_service/runner/impl/TwitterKafkaStreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/twitter_to_kafka_service/runner/impl/TwitterKafkaStreamRunner.java
@@ -6,6 +6,7 @@ import com.twitter_to_kafka_service.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import twitter4j.FilterQuery;
 import twitter4j.TwitterException;
@@ -14,11 +15,10 @@ import twitter4j.TwitterStreamFactory;
 
 import javax.annotation.PreDestroy;
 import java.util.Arrays;
-import java.util.logging.Filter;
 
 @Component
-//@ConditionalOnProperty(name = "twitter-to-kafka-service.enable-v2-tweets",havingValue = "true",matchIfMissing = true)
-@ConditionalOnExpression("${twitter-to-kafka-service.enable-mock-tweets} && not ${twitter-to-kafka-service.enable-v2-tweets}")
+@ConditionalOnProperty(name = "twitter-to-kafka-service.enable-v2-tweets",havingValue = "true",matchIfMissing = true)
+//@ConditionalOnExpression("${twitter-to-kafka-service.enable-mock-tweets} && not ${twitter-to-kafka-service.enable-v2-tweets}")
 public class TwitterKafkaStreamRunner implements StreamRunner {
     private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaStreamRunner.class);
 
