c2bbb7f4dd5efc96d235dba91608d217848d002f
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-10-18 22:20:48 +0530

    change main class to initialize kafka services

--- twitter-to-kafka-service/src/main/java/com/microservice/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservice/TwitterToKafkaServiceApplication.java
@@ -1,6 +1,6 @@
 package com.microservice;
 
-import com.microservice.config.ConfigData;
+import com.microservice.init.StreamInitializer;
 import com.microservice.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,13 +18,19 @@ application initialization logics
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
-    private final ConfigData configData;
+    //private final TwitterToKafkaServiceConfigData configData;
     private final StreamRunner streamRunner;
+    private final StreamInitializer streamInitializer;
 
-    public TwitterToKafkaServiceApplication(ConfigData configData,StreamRunner runner) {
+    public TwitterToKafkaServiceApplication(StreamRunner streamRunner, StreamInitializer streamInitializer) {
+        this.streamRunner = streamRunner;
+        this.streamInitializer = streamInitializer;
+    }
+
+    /*public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData,StreamRunner runner) {
         this.configData = configData;
         this.streamRunner = runner;
-    }
+    }*/
 
 
     public static void main(String[] args) {
@@ -32,18 +38,21 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     }
 
     /*
-    * executes after dependency injection to perform initialization
-    * */
+     * executes after dependency injection to perform initialization
+     * */
     /*@PostConstruct
     public void init(){
 
     }*/
     @Override
     public void run(String... args) throws Exception {
-       LOG.info("initiating");
-       LOG.info(String.valueOf(configData.getTwitterKeywords()));
-       LOG.info(configData.getWelcomeMessage());
-       // start streaming
+        LOG.info("initiating");
+        //LOG.info(String.valueOf(configData.getTwitterKeywords()));
+        //LOG.info(configData.getWelcomeMessage());
+
+        // initialize kafka topics and check stream registry
+        streamInitializer.init();
+        // start streaming
         streamRunner.start();
 
     }
