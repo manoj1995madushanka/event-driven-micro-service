c9546f0e7031cc595a123a2f5c874be8573eec76
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-10-19 23:30:10 +0530

    create BeanConfig class for init RetryTemplate bean

---
+++ twitter-to-kafka-service/src/main/java/com/event_driven_ms/config/BeanConfig.java
@@ -0,0 +1,14 @@
+package com.event_driven_ms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.support.RetryTemplate;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public RetryTemplate retryTemplate(){
+        return new RetryTemplate();
+    }
+}
