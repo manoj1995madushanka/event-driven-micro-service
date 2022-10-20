71f86e434a4f4ea2601a04e96c1a0026f1c1c709
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-10-20 08:19:09 +0530

    create bean config class

--- twitter-to-kafka-service/src/main/java/com/event_driven_ms/config/BeanConfig.java
+++ twitter-to-kafka-service/src/main/java/com/event_driven_ms/config/BeanConfig.java
@@ -1,5 +1,7 @@
 package com.event_driven_ms.config;
 
+import com.event_driven_ms.runner.StreamRunner;
+import com.event_driven_ms.runner.impl.TwitterKafkaStreamRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.support.RetryTemplate;
