e86b68e8253cd7c5a35ecd591c2de07a2c01ddb6
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-10-30 09:57:04 +0530

    create new feature branch for config server

--- twitter-to-kafka-service/src/main/java/com/event_driven_ms/init/StreamInitializer.java
+++
@@ -1,5 +0,0 @@
-package com.event_driven_ms.init;
-
-public interface StreamInitializer {
-    void init();
-}
--- twitter-to-kafka-service/src/main/java/com/event_driven_ms/runner/StreamRunner.java
+++
@@ -1,7 +0,0 @@
-package com.event_driven_ms.runner;
-
-import twitter4j.TwitterException;
-
-public interface StreamRunner {
-    void start() throws TwitterException;
-}
