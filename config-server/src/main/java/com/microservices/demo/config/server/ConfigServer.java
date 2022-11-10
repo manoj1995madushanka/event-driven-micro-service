79283bcefe6f289a8e5fadb6cc2a1389da3c9c2f
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-10 21:07:04 +0530

    configure services up order

--- config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
+++ config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
@@ -1,5 +1,6 @@
 package com.microservices.demo.config.server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
