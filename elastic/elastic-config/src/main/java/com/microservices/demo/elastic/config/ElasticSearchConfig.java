bafb54c04e4e819352a8338d43711d010f762772
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-19 16:45:46 +0530

    add elastic search config class and dependencies

---
+++ elastic/elastic-config/src/main/java/com/microservices/demo/elastic/config/ElasticSearchConfig.java
@@ -0,0 +1,43 @@
+package com.microservices.demo.elastic.config;
+
+import com.microservices.demo.config.ElasticConfigData;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Objects;
+
+@Configuration
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    private final ElasticConfigData elasticConfigData;
+
+    public ElasticSearchConfig(ElasticConfigData elasticConfigData) {
+        this.elasticConfigData = elasticConfigData;
+    }
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        UriComponents serverUri = UriComponentsBuilder.fromHttpUrl(elasticConfigData.getConnectionUrl()).build();
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost(
+                                Objects.requireNonNull(serverUri.getHost()),
+                                serverUri.getPort(),
+                                serverUri.getScheme()
+                        )
+                ).setRequestConfigCallback(
+                        requestConfigBuilder ->
+                                requestConfigBuilder
+                                        .setConnectTimeout(elasticConfigData.getConnectionTimeoutMs())
+                                        .setSocketTimeout(elasticConfigData.getSocketTimeoutMs())
+                )
+        );
+    }
+}
