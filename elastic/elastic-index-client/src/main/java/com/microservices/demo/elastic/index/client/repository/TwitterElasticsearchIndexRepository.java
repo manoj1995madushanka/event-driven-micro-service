7bbd10bb535fa1f49d9e320669535710c1d16f47
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-21 20:54:01 +0530

    note about elasticsearchrepository vs elasticsearchoperations

--- elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/repository/TwitterElasticsearchIndexRepository.java
+++ elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/repository/TwitterElasticsearchIndexRepository.java
@@ -4,6 +4,11 @@ import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+/*
+* ElasticSearchRepository provides convenient methods like save,find etc.
+* ElasticSearchOperations gives chance of running low level queries like elasticsearch bool, must etc.
+* ElasticSearchOperations requires to convert the input object to query objects
+* */
 @Repository
 public interface TwitterElasticsearchIndexRepository extends ElasticsearchRepository<TwitterIndexModel,String> {
 }
