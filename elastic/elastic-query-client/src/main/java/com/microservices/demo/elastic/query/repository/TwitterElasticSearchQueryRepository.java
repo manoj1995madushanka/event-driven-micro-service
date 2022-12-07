ee29f37ff6d3ad8a04581f8c7a1ba08279ebfab0
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-12-07 22:23:13 +0530

    add doc comment

--- elastic/elastic-query-client/src/main/java/com/microservices/demo/elastic/query/repository/TwitterElasticSearchQueryRepository.java
+++ elastic/elastic-query-client/src/main/java/com/microservices/demo/elastic/query/repository/TwitterElasticSearchQueryRepository.java
@@ -6,8 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * ElasticSearchRepository is a high level client that offers predefined
+ * methods to query against the fields on the elasticsearch.
+ * but it cannot be used to send complex quires to elasticsearch
+ */
 @Repository
-public interface TwitterElasticSearchQueryRepository extends ElasticsearchRepository<TwitterIndexModel,String> {
+public interface TwitterElasticSearchQueryRepository extends ElasticsearchRepository<TwitterIndexModel, String> {
 
     List<TwitterIndexModel> findByText(String text);
 }
