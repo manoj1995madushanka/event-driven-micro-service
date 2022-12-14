5a520bc76ee920b4f56e38cbd7328a537830c2c2
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-12-14 21:27:30 +0530

    add paths to ignore to security configuration

--- elastic-query-service/src/main/java/com/microservices/demo/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/demo/config/WebSecurityConfig.java
@@ -1,19 +1,24 @@
 package com.microservices.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserConfigData userConfigData;
+    @Value("${security.paths-to-ignore}")
+    private String[] pathsToIgnore;
 
     public WebSecurityConfig(UserConfigData userConfigData) {
         this.userConfigData = userConfigData;
@@ -25,6 +30,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .hasRole("USER").and()
                 .csrf()// csrf is an attack that uses already authenticated user's session to do unwanted actions, triggering it from browser
                 .disable();
+    }
+
+    @Override
+    public void configure( WebSecurity webSecurity) throws Exception {
+         webSecurity.ignoring().antMatchers(pathsToIgnore);
     }
 
     @Override
