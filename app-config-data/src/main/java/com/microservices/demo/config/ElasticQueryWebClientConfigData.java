1db1204084930534dc83f185eaaf6ece2bf0732a
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-12-16 23:43:46 +0530

    add configurations for elastic-query-web-client module

---
+++ app-config-data/src/main/java/com/microservices/demo/config/ElasticQueryWebClientConfigData.java
@@ -0,0 +1,28 @@
+package com.microservices.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "elastic-query-web-client")
+public class ElasticQueryWebClientConfigData {
+
+    private WebClient webClient;
+
+    /**
+     * we are using inner class because in config-client-elastic_query_web.yml
+     * has WebClient object as separate node
+     */
+    @Data
+    public static class WebClient {
+        private Integer connectTimeoutMs;
+        private Integer readTimeoutMs;
+        private Integer writeTimeoutMs;
+        private Integer maxInMemorySize;
+        private String contentType;
+        private String acceptType;
+        private String baseUrl;
+    }
+}
---
+++ elastic-query-web-client/src/test/java/com/microservices/demo/elastic/query/web/client/ElasticQueryWebClientApplicationTest.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.elastic.query.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ElasticQueryWebClientApplicationTest {
+
+    @Test
+    public void contextLoadTest(){
+
+    }
+
+}
