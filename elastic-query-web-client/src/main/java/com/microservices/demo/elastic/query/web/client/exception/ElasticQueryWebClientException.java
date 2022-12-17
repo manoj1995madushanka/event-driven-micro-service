0c4bcb6d2759a1f3641f90067be8a729675828b0
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-12-17 16:29:51 +0530

    add custom exception handler

---
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/exception/ElasticQueryWebClientException.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.elastic.query.web.client.exception;
+
+public class ElasticQueryWebClientException extends RuntimeException {
+
+    public ElasticQueryWebClientException() {
+        super();
+    }
+
+    public ElasticQueryWebClientException(String message) {
+        super(message);
+    }
+
+    public ElasticQueryWebClientException(String message, Throwable t) {
+        super(message, t);
+    }
+}
