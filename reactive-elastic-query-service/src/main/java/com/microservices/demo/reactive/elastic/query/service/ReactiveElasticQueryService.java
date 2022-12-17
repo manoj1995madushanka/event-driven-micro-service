c4220f8fc23905febad13fbe1e3f2b3b2297a76f
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-12-18 00:55:30 +0530

    create reactive-elastic-query-client

---
+++ reactive-elastic-query-service/src/main/java/com/microservices/demo/reactive/elastic/query/service/ReactiveElasticQueryService.java
@@ -0,0 +1,15 @@
+package com.microservices.demo.reactive.elastic.query.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.microservices.demo")
+public class ReactiveElasticQueryService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveElasticQueryService.class, args);
+
+    }
+}
---
+++ reactive-elastic-query-service/src/test/java/com/microservices/demo/reactive/elastic/query/service/ReactiveElasticQueryServiceTest.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.reactive.elastic.query.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ReactiveElasticQueryServiceTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+
+}
